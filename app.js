f2b7be05024c4d7a2030ff56b0c66ae4537cba26
mzunigau <mazuga92@gmail.com>
2020-07-31 18:17:02 -0600

    funciones de flecha

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use strict";
 (function () {
+    var miFuncion = function (a) {
+        return a;
+    };
+    var miFuncionF = function (a) { return a; };
+    var sumarN = function (a, b) {
+        return a + b;
+    };
+    var sumarF = function (a, b) { return a + b; };
+    console.log(sumarF(1, 2));
+    var hulk = {
+        nombre: 'Hulk',
+        smash: function () {
+            var _this = this;
+            setTimeout(function () {
+                console.log(" " + _this.nombre + " smash!!");
+            }, 1000);
+        }
+    };
+    hulk.smash();
 })();
