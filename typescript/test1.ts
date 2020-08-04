622497f6c059ce543bed321f99b3912178972e2f
mzunigau <mazuga92@gmail.com>
2020-08-03 19:01:08 -0600

    test con ejemplos de typescript

---
+++ typescript/test1.ts
@@ -0,0 +1,75 @@
+(()=>{
+
+  // Uso de Let y Const
+const nombre = "Ricardo Tapia";
+let edad = 23;
+let PERSONAJE = {
+  nombre: nombre,
+  edad: edad
+};
+ 
+ 
+interface Heroe{
+    nombre:string;
+    artesMarciales:string[];
+
+}
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+const batman:Heroe = {
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+ 
+ 
+// Convertir esta funcion a una funcion de flecha
+ const resultadoDoble=( a:number, b:number )  => (a + b) * 2;
+
+ 
+ 
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+const getAvenger=( nombre:string, poder?:string, arma:string="arco" ){
+  let mensaje:string;
+  if( poder ){
+     mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+  }else{
+     mensaje = nombre + " tiene un " + poder
+  }
+};
+ 
+ 
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+class rectangulo{
+
+  constructor(
+    public base:number,
+    public altura: number
+
+  ){}
+
+  
+  calcularArea():number{
+    return this.altura*this.base;
+   }
+
+
+
+}
+    
+
+    
+ 
+})();
+
+
+
